c71e013831ac44fbc5f0142de9ddc6e9f1cff2a0
xusniya123 <xusniya12345678@gmail.com>
2024-03-29 20:19:31 +0500

    4 moth Exam

---
+++ main.js
@@ -0,0 +1,92 @@
+// 1-savol Arifmetik operatorla
+//  5 sonini 2 ga bo'ling va qoldig`ini alertga chiqaring!
+// let a = 5; 
+// let b = 2;
+// let c;
+// c = a / b;
+// alert(c);
+
+// 2-savol  Math metodlari
+// 1 dan 10 gacha bo'lgan random son yasang va alertga chiqaring
+// const randomNumber = Math.floor(Math.random() * 10) + 1;
+// alert(randomNumber);
+
+// 3-savol Math metodlari
+// 12.510 sonini butun songa aylantiring!
+// let a = 12.510;
+// let b = Math.round(12,510);
+// alert(b);
+
+// 4-savol function
+// "MARS IT SCHOOL" sozini nechta harfdan iboratligini funksiya orqali topib va alertga chiqaring!.
+// function myFunction() {
+    //     var mars = "MARS IT SCHOOL";
+    //     var school = mars.replace(/\s/g, '').length;
+    //     alert("MARS IT SCHOOL so'zi " + school + " harfdan iborat.");
+    //   }
+      
+    //   myFunction();
+
+// 5-savol for loop
+// "MARS IT SCHOOL" sozini javascript funksiyasi orqali 10 marta console.log ga chiqaring!
+
+//     for (let i =0; i < 10; i++){
+// console.log("Mars IT School");
+// }
+
+// 6-savol Array methods
+// let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ]
+// console.log(harflar[21]);
+// console.log(harflar[19]);
+// console.log(harflar[17]);
+// console.log(harflar[12]);
+// console.log(harflar[7]);
+// console.log(harflar[22]);
+// console.log(harflar[0]);
+
+// 7-savol if else
+// Promtdan yosh kiriting agar yoshi 18 yoshdan katta bo'lsa console da Siz balag'ot yoshiga yetgansiz , aks holda  siz balog'at yoshiga yetmagansiz  // agar yoshi 18 yoshga teng bolsa  balog'at yoshi muborak deb console da chiqarish kerak
+// let age = prompt("Yoshingizni kiriting");
+// if (age > 18) {
+//     alert("Siz balag'ot yoshiga yetgansiz");
+// } else if (age >= 18) {
+//     alert("Balog'at yoshi muborak");
+// }
+
+// 8-savol String metodlari vs Array metodlari
+// Promtdan ismigizni kiriting va console.log da ismigizni teskarisini chiqaring
+// let name = prompt("Ismingizni kiriting");
+// let reverseName = name.split('').reverse().join('');
+
+// console.log(reverseName);
+
+// 9-savol Switch case
+// Promt 1 dan 7 gacha bo'lgan raqam kiriting va kiritilgan raqam qaysi hafta kuniga to'gri kelishini chiqarib bering
+// masalan 4 raqamini kiritsam console.log da("Siz kiritgan raqam haftaning Payshanba kuniga to'gri keladi") bo'lib chiqish kerak
+// agar kiritilgan raqam 7 dan katta bolsa ya'ni 8 yoki 15 kiritilsa console da (Namalum raqam kiritdingiz ) deb chiqish kerak.
+
+// 9ni biza hali o'tmaganmiz shunichun ustoz qimela didila
+
+
+// 10-savol Array metodlari
+// shu arraydagi juft sonlarni console ga chiqaring
+
+// let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// function isEven(num) {
+//   return num % 2 === 0;
+// }
+
+// console.log("Juft sonlar");
+// number.forEach(function(number) {
+//   if (isEven(number)) {
+//     console.log(number);
+//   }
+// });
+
+
+
+
+
+
+   
+
